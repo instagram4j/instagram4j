29293448d2bb4ca2b855fc033d5129a1da4a7e70
Bruno Volpato <brunocvcunha@gmail.com>
2017-05-06 21:46:38 -0700

    don't output password in the logs. closes #16

--- src/main/java/org/brunocvcunha/instagram4j/Instagram4j.java
+++ src/main/java/org/brunocvcunha/instagram4j/Instagram4j.java
@@ -128,7 +128,7 @@ public class Instagram4j {
      */
     public InstagramLoginResult login() throws ClientProtocolException, IOException {
         
-        log.info("Logging with user " + username + " and password " + password);
+        log.info("Logging with user " + username + " and password " + password.replaceAll(".", "*"));
         
         InstagramLoginPayload loginRequest = InstagramLoginPayload.builder().username(username)
                 .password(password)
