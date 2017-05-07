cd4685abbe2478117d401822ffafe71dd6b516a2
Bruno Volpato <brunocvcunha@gmail.com>
2017-05-06 23:37:48 -0700

    avoid compilation error on replace .

--- src/main/java/org/brunocvcunha/instagram4j/Instagram4j.java
+++ src/main/java/org/brunocvcunha/instagram4j/Instagram4j.java
@@ -128,7 +128,7 @@ public class Instagram4j {
      */
     public InstagramLoginResult login() throws ClientProtocolException, IOException {
         
-        log.info("Logging with user " + username + " and password " + password.replaceAll(".", "*"));
+        log.info("Logging with user " + username + " and password " + password.replaceAll("[a-zA-Z0-9]", "*"));
         
         InstagramLoginPayload loginRequest = InstagramLoginPayload.builder().username(username)
                 .password(password)
