ed2b6403a4b28853596b1a0cab35197564215d4e
Ozan Karaali <ozan.karaali@gmail.com>
2017-11-29 16:18:24 +0300

    Added UserStoryFeed and UserReelMediaFeed

---
+++ src/main/java/org/brunocvcunha/instagram4j/requests/InstagramGetUserReelMediaFeedRequest.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (C) 2016 Bruno Candido Volpato da Cunha (brunocvcunha@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.brunocvcunha.instagram4j.requests;
+
+import lombok.SneakyThrows;
+import org.brunocvcunha.instagram4j.requests.payload.InstagramGetUserReelMediaFeedResult;
+
+/**
+ * Get User Reel Media Feed equest
+ *
+ * @author Ozan Karaali
+ *
+ */
+
+public class InstagramGetUserReelMediaFeedRequest extends InstagramGetRequest<InstagramGetUserReelMediaFeedResult>{
+    @Override
+    public String getPayload() {
+        return null;
+    }
+
+    @Override
+    public String getUrl() {
+        return "feed/user/{$userId}/reel_media/?";
+    }
+
+    @Override
+    @SneakyThrows
+    public InstagramGetUserReelMediaFeedResult parseResult(int resultCode, String content) {
+        return parseJson(resultCode, content, InstagramGetUserReelMediaFeedResult.class);
+    }
+}
---
+++ src/main/java/org/brunocvcunha/instagram4j/requests/payload/InstagramGetUserReelMediaFeedResult.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (C) 2016 Bruno Candido Volpato da Cunha (brunocvcunha@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.brunocvcunha.instagram4j.requests.payload;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+public class InstagramGetUserReelMediaFeedResult extends StatusResult {
+    private String id;
+    private List<String> items; //item
+    private InstagramUser user;
+    private long expiring_at;
+    private Object seen; //boolean?
+    private Object can_reply; //boolean
+    private Object location;
+    private Object latest_reel_media;
+    private Object prefetch_count;
+    private InstagramBroadcast broadcast;
+}
