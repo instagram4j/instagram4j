4169da6d944608677389d0cefa6ab4fcc8574046
Mahir Köle <mahirkole@gmail.com>
2018-07-22 00:45:44 +0300

    Update InstagramTimelineFeedRequest.java

--- src/main/java/org/brunocvcunha/instagram4j/requests/InstagramTimelineFeedRequest.java
+++ src/main/java/org/brunocvcunha/instagram4j/requests/InstagramTimelineFeedRequest.java
@@ -13,8 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.brunocvcunha.instagram4j.requests;
+
 import org.brunocvcunha.instagram4j.requests.InstagramGetRequest;
 import org.brunocvcunha.instagram4j.requests.payload.InstagramFeedResult;
+
 import lombok.SneakyThrows;
 
 /**
