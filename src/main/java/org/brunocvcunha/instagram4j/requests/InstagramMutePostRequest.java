c1687ef73599c21eda1b1be8ba3f5523c60ba91c
rodrigae <30932608+rodrigae@users.noreply.github.com>
2019-04-03 14:40:38 -0500

    changed it to return InstagramFriendshipStatus, instead of Postcomment, I found it easier to get status of the muting.
    Thanks to the user who created this. amazing!

---
+++ src/main/java/org/brunocvcunha/instagram4j/requests/InstagramMutePostRequest.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (C) 2016 Bruno Candido Volpato da Cunha (brunocvcunha@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.brunocvcunha.instagram4j.requests;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j;
+import org.brunocvcunha.instagram4j.requests.payload.InstagramFriendshipStatus;
+import org.brunocvcunha.instagram4j.requests.payload.InstagramPostCommentResult;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Comment Post Request
+ *
+ * @author Malloum LAYA & Stephane Sabalot
+ *
+ */
+@AllArgsConstructor
+@Log4j
+public class InstagramMutePostRequest extends InstagramPostRequest<InstagramFriendshipStatus> {
+
+    private Long userId;
+
+    @Override
+    public String getUrl() {
+        return "friendships/mute_posts_or_story_from_follow/";
+    }
+
+    @Override
+    @SneakyThrows
+    public String getPayload() {
+
+        Map<String, Object> likeMap = new LinkedHashMap<>();
+        likeMap.put("_uuid", api.getUuid());
+        likeMap.put("_uid", api.getUserId());
+        likeMap.put("_csrftoken", api.getOrFetchCsrf());
+        likeMap.put("target_posts_author_id", userId);
+        ObjectMapper mapper = new ObjectMapper();
+        String payloadJson = mapper.writeValueAsString(likeMap);
+
+
+
+
+
+        return payloadJson;
+    }
+
+    @Override
+    @SneakyThrows
+    public InstagramFriendshipStatus parseResult(int statusCode, String content) {
+        return parseJson(statusCode, content, InstagramFriendshipStatus.class);
+    }
+
+
+}
