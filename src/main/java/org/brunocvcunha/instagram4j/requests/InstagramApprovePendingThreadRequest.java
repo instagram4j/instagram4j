54629563922515234bf1c99144960340f529b8d9
jemand771 <jemand771@gmx.net>
2019-05-08 22:29:19 +0200

    added ability to approve dm requests in pending inbox

---
+++ src/main/java/org/brunocvcunha/instagram4j/requests/InstagramApprovePendingThreadRequest.java
@@ -0,0 +1,47 @@
+package org.brunocvcunha.instagram4j.requests;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.brunocvcunha.instagram4j.requests.InstagramGetRequest;
+import org.brunocvcunha.instagram4j.requests.InstagramPostRequest;
+import org.brunocvcunha.instagram4j.requests.payload.StatusResult;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class InstagramApprovePendingThreadRequest extends InstagramPostRequest<StatusResult> {
+
+    private String threadId;
+
+    @Override
+    public String getUrl() {
+
+        String baseUrl = "direct_v2/threads/" + threadId + "/approve/";
+        return baseUrl;
+    }
+
+    @Override
+    @SneakyThrows
+    public String getPayload() {
+
+        Map<String, Object> approveMap = new LinkedHashMap<>();
+        approveMap.put("_uuid", api.getUuid());
+        approveMap.put("_csrftoken", api.getOrFetchCsrf());
+        approveMap.put("_uid", api.getUserId());
+
+        ObjectMapper mapper = new ObjectMapper();
+        String payloadJson = mapper.writeValueAsString(approveMap);
+
+        return payloadJson;
+    }
+
+    @Override
+    @SneakyThrows
+    public StatusResult parseResult(int statusCode, String content) {
+        return parseJson(statusCode, content, StatusResult.class);
+    }
+}
