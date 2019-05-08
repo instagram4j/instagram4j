b05fd2c24aa162dd7ec3a6b6237b8664e3b9ff8b
jemand771 <jemand771@gmx.net>
2019-05-08 22:28:03 +0200

    added request to get "hidden" instagram inbox with pending direct messages. new messages by users you have never texted with or followed get put here instead of the normal InstagramInbox retrieved by InstagramInboxRequest

---
+++ src/main/java/org/brunocvcunha/instagram4j/requests/payload/InstagramPendingInboxResult.java
@@ -0,0 +1,20 @@
+package org.brunocvcunha.instagram4j.requests.payload;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Result object for the "hidden" instagram inbox containing pending direct messages
+ *
+ * @author Willy Hille
+ */
+@Getter
+@Setter
+@ToString(callSuper = true)
+public class InstagramPendingInboxResult extends StatusResult {
+
+    public String seq_id;
+    public int pending_requests_total;
+    public InstagramInbox inbox;
+}
