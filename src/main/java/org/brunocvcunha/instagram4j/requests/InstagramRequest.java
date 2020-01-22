93a8dd6014495582689779ee2b16c2d53f043b9c
jvogit <justinxvopro@gmail.com>
2020-01-22 02:40:51 -0800

    Fix formatting

--- src/main/java/org/brunocvcunha/instagram4j/requests/InstagramRequest.java
+++ src/main/java/org/brunocvcunha/instagram4j/requests/InstagramRequest.java
@@ -46,139 +46,144 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public abstract class InstagramRequest<T> {
 
-    @Getter @Setter
-    @JsonIgnore
-    protected Instagram4j api;
-    
-    /**
-     * @return the url
-     */
-    public abstract String getUrl();
-    
-    /**
-     * @return the method
-     */
-    public abstract String getMethod();
-    
-    /**
-     * @return the payload
-     */
-    public String getPayload() {
-        return null;
-    }
-    
-    public HttpEntity getPayloadEntity() throws UnsupportedEncodingException {
-    	return null;
-    }
-    
-    /**
-     * @return the result
-     * @throws IOException 
-     * @throws ClientProtocolException 
-     */
-    public abstract T execute() throws ClientProtocolException, IOException;
-    
-    /**
-     * Process response
-     * @param resultCode Status Code
-     * @param content Content
-     */
-    public abstract T parseResult(int resultCode, String content);
-    
-    /**
-     * @return if request must be logged in
-     */
-    public boolean requiresLogin() {
-        return true;
-    }
+	@Getter
+	@Setter
+	@JsonIgnore
+	protected Instagram4j api;
 
-    /**
-     * Parses Json into type, considering the status code
-     * @param statusCode HTTP Status Code
-     * @param str Entity content
-     * @param clazz Class
-     * @return Result
-     */
-    @SneakyThrows
-    public <U> U parseJson(int statusCode, String str, Class<U> clazz) {
-        
-        if (clazz.isAssignableFrom(StatusResult.class)) {
-            
-            //TODO: implement a better way to handle exceptions
-            if (statusCode == HttpStatus.SC_NOT_FOUND) {
-                StatusResult result = (StatusResult) clazz.newInstance();
-                result.setStatus("error");
-                result.setMessage("SC_NOT_FOUND");
-                return (U) result;
-            } else if (statusCode == HttpStatus.SC_FORBIDDEN) {
-                StatusResult result = (StatusResult) clazz.newInstance();
-                result.setStatus("error");
-                result.setMessage("SC_FORBIDDEN");
-                return (U) result;
-            }
-}
-        
-        return parseJson(str, clazz);
-    }
-    
-    /**
-     * Parses Json into type
-     * @param str Entity content
-     * @param clazz Class
-     * @return Result
-     */
-    @SneakyThrows
-    public <U> U parseJson(String str, Class<U> clazz) {
-        
-        if (log.isInfoEnabled()) {
-            
-            if (log.isDebugEnabled()) {
-                log.debug("Reading " + clazz.getSimpleName() + " from " + str);
-            } else {
-                String printStr = str;
-                if (printStr.length() > 128) {
-                    printStr = printStr.substring(0, 128);
-                }
-                log.info("Reading " + clazz.getSimpleName() + " from " + printStr);
-            }
-            
-        }
-        
-        ObjectMapper objectMapper = new ObjectMapper()
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-                .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+	/**
+	 * @return the url
+	 */
+	public abstract String getUrl();
 
-        return objectMapper.readValue(str, clazz);
-    }
-    
-    /**
-     * Parses Json into type
-     * @param is Entity stream
-     * @param clazz Class
-     * @return Result
-     */
-    @SneakyThrows
-    public T parseJson(InputStream is, Class<T> clazz) {
-        return this.parseJson(MyStreamUtils.readContent(is), clazz);
-    }
+	/**
+	 * @return the method
+	 */
+	public abstract String getMethod();
 
-    /**
-     * @return payload should be signed
-     */
-    public boolean isSigned() {
-        return true;
-    }
-    
-    public <E extends HttpRequest> E applyHeaders(E  req) {
-    	
-    	req.addHeader("Connection", "close");
-        req.addHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-        req.addHeader("Accept-Language", "en-US");
-        req.addHeader("X-IG-Capabilities", InstagramConstants.DEVICE_CAPABILITIES);
-        req.addHeader("X-IG-App-ID", InstagramConstants.APP_ID);
-        req.addHeader("User-Agent", InstagramConstants.USER_AGENT);
-        req.addHeader("X-IG-Connection-Type", "WIFI");
-        req.addHeader("X-Ads-Opt-Out", "0");
+	/**
+	 * @return the payload
+	 */
+	public String getPayload() {
+		return null;
+	}
+
+	public HttpEntity getPayloadEntity() throws UnsupportedEncodingException {
+		return null;
+	}
+
+	/**
+	 * @return the result
+	 * @throws IOException
+	 * @throws ClientProtocolException
+	 */
+	public abstract T execute() throws ClientProtocolException, IOException;
+
+	/**
+	 * Process response
+	 * 
+	 * @param resultCode Status Code
+	 * @param content    Content
+	 */
+	public abstract T parseResult(int resultCode, String content);
+
+	/**
+	 * @return if request must be logged in
+	 */
+	public boolean requiresLogin() {
+		return true;
+	}
+
+	/**
+	 * Parses Json into type, considering the status code
+	 * 
+	 * @param statusCode HTTP Status Code
+	 * @param str        Entity content
+	 * @param clazz      Class
+	 * @return Result
+	 */
+	@SneakyThrows
+	public <U> U parseJson(int statusCode, String str, Class<U> clazz) {
+
+		if (clazz.isAssignableFrom(StatusResult.class)) {
+
+			// TODO: implement a better way to handle exceptions
+			if (statusCode == HttpStatus.SC_NOT_FOUND) {
+				StatusResult result = (StatusResult) clazz.newInstance();
+				result.setStatus("error");
+				result.setMessage("SC_NOT_FOUND");
+				return (U) result;
+			} else if (statusCode == HttpStatus.SC_FORBIDDEN) {
+				StatusResult result = (StatusResult) clazz.newInstance();
+				result.setStatus("error");
+				result.setMessage("SC_FORBIDDEN");
+				return (U) result;
+			}
+		}
+
+		return parseJson(str, clazz);
+	}
+
+	/**
+	 * Parses Json into type
+	 * 
+	 * @param str   Entity content
+	 * @param clazz Class
+	 * @return Result
+	 */
+	@SneakyThrows
+	public <U> U parseJson(String str, Class<U> clazz) {
+
+		if (log.isInfoEnabled()) {
+
+			if (log.isDebugEnabled()) {
+				log.debug("Reading " + clazz.getSimpleName() + " from " + str);
+			} else {
+				String printStr = str;
+				if (printStr.length() > 128) {
+					printStr = printStr.substring(0, 128);
+				}
+				log.info("Reading " + clazz.getSimpleName() + " from " + printStr);
+			}
+
+		}
+
+		ObjectMapper objectMapper = new ObjectMapper()
+				.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+				.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+
+		return objectMapper.readValue(str, clazz);
+	}
+
+	/**
+	 * Parses Json into type
+	 * 
+	 * @param is    Entity stream
+	 * @param clazz Class
+	 * @return Result
+	 */
+	@SneakyThrows
+	public T parseJson(InputStream is, Class<T> clazz) {
+		return this.parseJson(MyStreamUtils.readContent(is), clazz);
+	}
+
+	/**
+	 * @return payload should be signed
+	 */
+	public boolean isSigned() {
+		return true;
+	}
+
+	public <E extends HttpRequest> E applyHeaders(E req) {
+
+		req.addHeader("Connection", "close");
+		req.addHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+		req.addHeader("Accept-Language", "en-US");
+		req.addHeader("X-IG-Capabilities", InstagramConstants.DEVICE_CAPABILITIES);
+		req.addHeader("X-IG-App-ID", InstagramConstants.APP_ID);
+		req.addHeader("User-Agent", InstagramConstants.USER_AGENT);
+		req.addHeader("X-IG-Connection-Type", "WIFI");
+		req.addHeader("X-Ads-Opt-Out", "0");
 		req.addHeader("X-CM-Bandwidth-KBPS", "-1.000");
 		req.addHeader("X-CM-Latency", "-1.000");
 		req.addHeader("X-IG-App-Locale", "en_US");
@@ -194,8 +199,8 @@ public abstract class InstagramRequest<T> {
 		req.addHeader("X-MID", api.getX_MID());
 		req.addHeader("X-IG-WWW-Claim", api.getWWW_CLAIM());
 		req.addHeader("X-FB-HTTP-engine", "Liger");
-		
-    	return req;
-    }
-    
+
+		return req;
+	}
+
 }
--- src/main/java/org/brunocvcunha/instagram4j/requests/internal/InstagramUploadMediaFinishRequest.java
+++ src/main/java/org/brunocvcunha/instagram4j/requests/internal/InstagramUploadMediaFinishRequest.java
@@ -26,37 +26,37 @@ public class InstagramUploadMediaFinishRequest extends InstagramPostRequest<Stat
 	@NonNull
 	private String sourceType;
 	private Collection<Entry<String, Object>> extraParams;
-	
+
 	@Override
 	public String getUrl() {
 		return "media/upload_finish/?video=1";
 	}
-	
+
 	@Override
 	public boolean isSigned() {
 		return true;
 	}
-	
+
 	@Override
 	@SneakyThrows
 	public String getPayload() {
 		ObjectMapper mapper = new ObjectMapper();
 		Map<String, Object> map = new HashMap<>();
-        map.put("_csrftoken", api.getOrFetchCsrf());
-        map.put("timezone_offset", String.valueOf(TimeZone.getDefault().getRawOffset()));
-        map.put("_uid", api.getUserId());
-        map.put("_uuid", api.getUuid());
-        map.put("upload_id", uploadId);
-        map.put("source_type", sourceType);
-        map.put("device_id", api.getDeviceId());
-        Map<String, Object> deviceMap = new LinkedHashMap<>();
-        deviceMap.put("manufacturer", InstagramConstants.getDevice().getDEVICE_MANUFACTURER());
-        deviceMap.put("model", InstagramConstants.getDevice().getDEVICE_MODEL());
-        deviceMap.put("android_version", InstagramConstants.getDevice().getDEVICE_ANDROID_VERSION());
-        deviceMap.put("android_release", InstagramConstants.getDevice().getDEVICE_ANDROID_RELEASE());
-        map.put("device", deviceMap);
-        extraParams.forEach(e -> map.put(e.getKey(), e.getValue()));
-        
+		map.put("_csrftoken", api.getOrFetchCsrf());
+		map.put("timezone_offset", String.valueOf(TimeZone.getDefault().getRawOffset()));
+		map.put("_uid", api.getUserId());
+		map.put("_uuid", api.getUuid());
+		map.put("upload_id", uploadId);
+		map.put("source_type", sourceType);
+		map.put("device_id", api.getDeviceId());
+		Map<String, Object> deviceMap = new LinkedHashMap<>();
+		deviceMap.put("manufacturer", InstagramConstants.getDevice().getDEVICE_MANUFACTURER());
+		deviceMap.put("model", InstagramConstants.getDevice().getDEVICE_MODEL());
+		deviceMap.put("android_version", InstagramConstants.getDevice().getDEVICE_ANDROID_VERSION());
+		deviceMap.put("android_release", InstagramConstants.getDevice().getDEVICE_ANDROID_RELEASE());
+		map.put("device", deviceMap);
+		extraParams.forEach(e -> map.put(e.getKey(), e.getValue()));
+
 		return mapper.writeValueAsString(map);
 	}
 
