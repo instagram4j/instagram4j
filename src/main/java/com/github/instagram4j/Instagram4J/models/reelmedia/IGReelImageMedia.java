c3126da454e7b6c19aab805ab7c47ced68b7e0d7
jvogit <justinxvopro@gmail.com>
2020-06-23 13:25:08 -0700

    Add IGReelMedia models

---
+++ src/main/java/com/github/instagram4j/Instagram4J/models/reelmedia/IGReelImageMedia.java
@@ -0,0 +1,8 @@
+package com.github.instagram4j.Instagram4J.models.reelmedia;
+
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
+@JsonTypeName("1")
+public class IGReelImageMedia extends IGReelMedia {
+	
+}
--- src/main/java/com/github/instagram4j/Instagram4J/models/reelmedia/IGReelMedia.java
+++ src/main/java/com/github/instagram4j/Instagram4J/models/reelmedia/IGReelMedia.java
@@ -1,11 +1,16 @@
-package com.github.instagram4j.Instagram4J.models.media;
+package com.github.instagram4j.Instagram4J.models.reelmedia;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.github.instagram4j.Instagram4J.models.media.IGMedia;
 
 import lombok.Data;
 
 @Data
 @JsonTypeInfo(defaultImpl=IGReelMedia.class, use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "media_type", visible = true)
+@JsonSubTypes({
+	@JsonSubTypes.Type(value = IGReelImageMedia.class)
+})
 public class IGReelMedia extends IGMedia {
 	private int viewer_count;
 }
