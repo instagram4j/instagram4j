d00fbf057a4edde1020165ed04eb1cf8d0e84979
jvogit <justinxvopro@gmail.com>
2020-06-24 01:52:28 -0700

    change spacing

--- src/main/java/com/github/instagram4j/Instagram4J/responses/IGResponse.java
+++ src/main/java/com/github/instagram4j/Instagram4J/responses/IGResponse.java
@@ -11,16 +11,16 @@ import lombok.Data;
 public class IGResponse {
 	@JsonAnySetter
 	private Map<String, Object> extra_properties = new HashMap<>();
-    private String status;
-    private String message;
-    private boolean spam;
-    private boolean lock;
-    private String feedback_title;
-    private String feedback_message;
-    private String error_type;
-    private String checkpoint_url;
-    
-    public Object getExtraProperty(String key) {
-    	return extra_properties.get(key);
-    }
+	private String status;
+	private String message;
+	private boolean spam;
+	private boolean lock;
+	private String feedback_title;
+	private String feedback_message;
+	private String error_type;
+	private String checkpoint_url;
+
+	public Object getExtraProperty(String key) {
+		return extra_properties.get(key);
+	}
 }
