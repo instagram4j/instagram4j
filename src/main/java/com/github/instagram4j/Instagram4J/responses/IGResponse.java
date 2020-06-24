4b4eb1604302b4b266aac79aa5756aae414bb775
jvogit <justinxvopro@gmail.com>
2020-06-24 01:49:29 -0700

    add extra properties for response model

--- src/main/java/com/github/instagram4j/Instagram4J/responses/IGResponse.java
+++ src/main/java/com/github/instagram4j/Instagram4J/responses/IGResponse.java
@@ -1,9 +1,16 @@
 package com.github.instagram4j.Instagram4J.responses;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+
 import lombok.Data;
 
 @Data
 public class IGResponse {
+	@JsonAnySetter
+	private Map<String, Object> extra_properties = new HashMap<>();
     private String status;
     private String message;
     private boolean spam;
@@ -12,4 +19,8 @@ public class IGResponse {
     private String feedback_message;
     private String error_type;
     private String checkpoint_url;
+    
+    public Object getExtraProperty(String key) {
+    	return extra_properties.get(key);
+    }
 }
