75a2f360d38fb96e3a48a7a73b35200a77b4f447
jvogit <justinxvopro@gmail.com>
2020-07-08 01:33:43 -0700

    modify: remove unnecessary encoding

--- src/main/java/com/github/instagram4j/Instagram4J/requests/feed/IGFeedTagRequest.java
+++ src/main/java/com/github/instagram4j/Instagram4J/requests/feed/IGFeedTagRequest.java
@@ -1,14 +1,11 @@
 package com.github.instagram4j.Instagram4J.requests.feed;
 
-import java.net.URLEncoder;
-
 import com.github.instagram4j.Instagram4J.requests.IGGetRequest;
 import com.github.instagram4j.Instagram4J.responses.feed.IGFeedTagResponse;
 
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 
 @RequiredArgsConstructor
 @AllArgsConstructor
@@ -17,10 +14,9 @@ public class IGFeedTagRequest extends IGGetRequest<IGFeedTagResponse> {
     private String tag;
     private String max_id;
 
-    @Override
-    @SneakyThrows
+    @Override       
     public String path() {
-        return "feed/tag/" + URLEncoder.encode(tag, "utf-8") + "/";
+        return "feed/tag/" + tag + "/";
     }
     
     @Override
