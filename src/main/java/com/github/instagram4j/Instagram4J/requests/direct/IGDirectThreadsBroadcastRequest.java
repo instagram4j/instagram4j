129a2c978a533775a3517fbb3c748f6954c6714e
jvogit <justinxvopro@gmail.com>
2020-07-16 01:11:41 -0700

    add: link payload request
    modify: change broadcast payload to accommadate multiple threads and
    users

--- src/main/java/com/github/instagram4j/Instagram4J/requests/direct/IGDirectThreadsBroadcastRequest.java
+++ src/main/java/com/github/instagram4j/Instagram4J/requests/direct/IGDirectThreadsBroadcastRequest.java
@@ -1,6 +1,10 @@
 package com.github.instagram4j.Instagram4J.requests.direct;
 
+import java.util.Arrays;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.github.instagram4j.Instagram4J.models.IGPayload;
 import com.github.instagram4j.Instagram4J.requests.IGPostRequest;
 import com.github.instagram4j.Instagram4J.responses.IGResponse;
@@ -30,13 +34,37 @@ public class IGDirectThreadsBroadcastRequest extends IGPostRequest<IGResponse> {
     public Class<IGResponse> getResponseType() {
         return IGResponse.class;
     }
+    
+    @Data
+    @JsonInclude(Include.NON_NULL)
+    public static abstract class IGBroadcastPayload extends IGPayload {
+        private String action = "send_item";
+
+        @JsonIgnore
+        public abstract String getItemType();
+
+        public abstract String getThread_ids();
+        
+        public abstract String getRecipient_users();
+    }
 
     @Data
     public static class IGBroadcastTextPayload extends IGBroadcastPayload {
-        @NonNull
-        private String thread_id;
-        @NonNull
-        private String text;
+        private final String thread_ids;
+        private final String recipient_users;
+        private final String text;
+        
+        public IGBroadcastTextPayload(String text, long... pks) {
+            this.thread_ids = null;
+            this.recipient_users = IGUtils.objectToJson(Arrays.asList(pks));
+            this.text = text;
+        }
+        
+        public IGBroadcastTextPayload(String text, String... thread_ids) {
+            this.thread_ids = IGUtils.objectToJson(thread_ids);
+            this.recipient_users = null;
+            this.text = text;
+        }
 
         public String getItemType() {
             return "text";
@@ -45,11 +73,26 @@ public class IGDirectThreadsBroadcastRequest extends IGPostRequest<IGResponse> {
 
     @Data
     public static class IGBroadcastMediaSharePayload extends IGBroadcastPayload {
-        @NonNull
-        private String thread_id;
-        @NonNull
-        private String media_id;
-
+        private final String thread_ids;
+        private final String recipient_users;
+        private final String text;
+        private final String media_id;
+        
+        public IGBroadcastMediaSharePayload(String media_id, String text, long... pks) {
+            this.thread_ids = null;
+            this.recipient_users = IGUtils.objectToJson(Arrays.asList(pks));
+            this.media_id = media_id;
+            this.text = text;
+        }
+        
+        public IGBroadcastMediaSharePayload(String media_id, String text, String... thread_ids) {
+            this.thread_ids = IGUtils.objectToJson(thread_ids);
+            this.recipient_users = null;
+            this.media_id = media_id;
+            this.text = text;
+        }
+        
+        @Override
         public String getItemType() {
             return "media_share";
         }
@@ -57,12 +100,24 @@ public class IGDirectThreadsBroadcastRequest extends IGPostRequest<IGResponse> {
 
     @Data
     public static class IGBroadcastReelSharePayload extends IGBroadcastPayload {
-        @NonNull
-        private String thread_id;
-        @NonNull
-        private String media_id;
-        @NonNull
-        private String text;
+        private final String thread_ids;
+        private final String recipient_users;
+        private final String text;
+        private final String media_id;
+        
+        public IGBroadcastReelSharePayload(String media_id, String text, long... pks) {
+            this.thread_ids = null;
+            this.recipient_users = IGUtils.objectToJson(Arrays.asList(pks));
+            this.media_id = media_id;
+            this.text = text;
+        }
+        
+        public IGBroadcastReelSharePayload(String media_id, String text, String... thread_ids) {
+            this.thread_ids = IGUtils.objectToJson(thread_ids);
+            this.recipient_users = null;
+            this.media_id = media_id;
+            this.text = text;
+        }
 
         @Override
         public String getItemType() {
@@ -71,15 +126,26 @@ public class IGDirectThreadsBroadcastRequest extends IGPostRequest<IGResponse> {
     }
 
     @Data
-    @RequiredArgsConstructor
-    @AllArgsConstructor
     public static class IGBroadcastStorySharePayload extends IGBroadcastPayload {
-        @NonNull
-        private String thread_id;
-        @NonNull
-        private String story_media_id;
-        private String text = "";
-
+        private final String thread_ids;
+        private final String recipient_users;
+        private final String story_media_id;
+        private final String text;
+        
+        public IGBroadcastStorySharePayload(String media_id, String text, long... pks) {
+            this.thread_ids = null;
+            this.recipient_users = IGUtils.objectToJson(Arrays.asList(pks));
+            this.story_media_id = media_id;
+            this.text = text;
+        }
+        
+        public IGBroadcastStorySharePayload(String media_id, String text, String... thread_ids) {
+            this.thread_ids = IGUtils.objectToJson(thread_ids);
+            this.recipient_users = null;
+            this.story_media_id = media_id;
+            this.text = text;
+        }
+        
         @Override
         public String getItemType() {
             return "story_share";
@@ -87,15 +153,27 @@ public class IGDirectThreadsBroadcastRequest extends IGPostRequest<IGResponse> {
     }
 
     @Data
+    @AllArgsConstructor
     public static class IGBroadcastShareVoicePayload extends IGBroadcastPayload {
-        @NonNull
-        private String thread_id;
-        @NonNull
-        private String upload_id;
-        private int waveform_sampling_frequency_hz = 10;
+        private final String thread_ids;
+        private final String recipient_users;
+        private final String upload_id;
         private String waveform = IGUtils.objectToJson(new double[] { 0.146, 0.5, 0.854, 1, 0.854, 0.5, 0.146, 0, 0.146,
                 0.5, 0.854, 1, 0.854, 0.5, 0.146, 0, 0.146, 0.5, 0.854, 1 });
-
+        private int waveform_sampling_frequency_hz = 10;
+        
+        public IGBroadcastShareVoicePayload(String upload_id, long... pks) {
+            this.thread_ids = null;
+            this.recipient_users = IGUtils.objectToJson(Arrays.asList(pks));
+            this.upload_id = upload_id;
+        }
+        
+        public IGBroadcastShareVoicePayload(String upload_id, String... upload_ids) {
+            this.thread_ids = IGUtils.objectToJson(upload_ids);
+            this.recipient_users = null;
+            this.upload_id = upload_id;
+        }
+        
         @Override
         public String getItemType() {
             return "share_voice";
@@ -104,11 +182,22 @@ public class IGDirectThreadsBroadcastRequest extends IGPostRequest<IGResponse> {
 
     @Data
     public static class IGBroadcastProfilePayload extends IGBroadcastPayload {
-        @NonNull
-        private String thread_id;
-        @NonNull
-        private String profile_user_id;
-
+        private final String thread_ids;
+        private final String recipient_users;
+        private final String profile_user_id;
+        
+        public IGBroadcastProfilePayload(String user_id, long... pks) {
+            this.thread_ids = null;
+            this.recipient_users = IGUtils.objectToJson(Arrays.asList(pks));
+            this.profile_user_id = user_id;
+        }
+        
+        public IGBroadcastProfilePayload(String user_id, String... thread_ids) {
+            this.thread_ids = IGUtils.objectToJson(thread_ids);
+            this.recipient_users = null;
+            this.profile_user_id = user_id;
+        }
+        
         public String getItemType() {
             return "profile";
         }
@@ -116,11 +205,22 @@ public class IGDirectThreadsBroadcastRequest extends IGPostRequest<IGResponse> {
 
     @Data
     public static class IGBroadcastConfigurePhotoPayload extends IGBroadcastPayload {
-        @NonNull
-        private String thread_id;
-        @NonNull
-        private String upload_id;
-
+        private final String thread_ids;
+        private final String recipient_users;
+        private final String upload_id;
+        
+        public IGBroadcastConfigurePhotoPayload(String upload_id, long... pks) {
+            this.thread_ids = null;
+            this.recipient_users = IGUtils.objectToJson(Arrays.asList(pks));
+            this.upload_id = upload_id;
+        }
+        
+        public IGBroadcastConfigurePhotoPayload(String upload_id, String... thread_ids) {
+            this.thread_ids = IGUtils.objectToJson(thread_ids);
+            this.recipient_users = null;
+            this.upload_id = upload_id;
+        }
+        
         public String getItemType() {
             return "configure_photo";
         }
@@ -128,24 +228,51 @@ public class IGDirectThreadsBroadcastRequest extends IGPostRequest<IGResponse> {
 
     @Data
     public static class IGBroadcastConfigureVideoPayload extends IGBroadcastPayload {
-        @NonNull
-        private String thread_id;
-        @NonNull
-        private String upload_id;
-
+        private final String thread_ids;
+        private final String recipient_users;
+        private final String upload_id;
+        
+        public IGBroadcastConfigureVideoPayload(String upload_id, long... pks) {
+            this.thread_ids = null;
+            this.recipient_users = IGUtils.objectToJson(Arrays.asList(pks));
+            this.upload_id = upload_id;
+        }
+        
+        public IGBroadcastConfigureVideoPayload(String upload_id, String... thread_ids) {
+            this.thread_ids = IGUtils.objectToJson(thread_ids);
+            this.recipient_users = null;
+            this.upload_id = upload_id;
+        }
+        
         public String getItemType() {
             return "configure_video";
         }
     }
-
+    
     @Data
-    public static abstract class IGBroadcastPayload extends IGPayload {
-        private String action = "send_item";
-
-        @JsonIgnore
-        public abstract String getItemType();
-
-        public abstract String getThread_id();
+    public static class IGBroadcastLinkPayload extends IGBroadcastPayload {
+        private final String thread_ids;
+        private final String recipient_users;
+        private final String link_text;
+        private final String link_urls;
+        
+        public IGBroadcastLinkPayload(String text, String[] link_urls, long... pks) {
+            this.thread_ids = null;
+            this.recipient_users = IGUtils.objectToJson(Arrays.asList(pks));
+            this.link_text = text;
+            this.link_urls = IGUtils.objectToJson(link_urls);
+        }
+        
+        public IGBroadcastLinkPayload(String text, String[] link_urls, String... thread_ids) {
+            this.thread_ids = IGUtils.objectToJson(thread_ids);
+            this.recipient_users = null;
+            this.link_text = text;
+            this.link_urls = IGUtils.objectToJson(link_urls);        
+        }
+        
+        public String getItemType() {
+            return "link";
+        }
     }
 
 }
