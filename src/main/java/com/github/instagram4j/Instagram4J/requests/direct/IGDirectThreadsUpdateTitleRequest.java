104e63c1fe26fb2f471d9caa2f31a9d2f4e80c8a
jvogit <justinxvopro@gmail.com>
2020-07-16 02:41:25 -0700

    add: direct threads update title request

---
+++ src/main/java/com/github/instagram4j/Instagram4J/requests/direct/IGDirectThreadsUpdateTitleRequest.java
@@ -0,0 +1,36 @@
+package com.github.instagram4j.Instagram4J.requests.direct;
+
+import com.github.instagram4j.Instagram4J.models.IGPayload;
+import com.github.instagram4j.Instagram4J.requests.IGPostRequest;
+import com.github.instagram4j.Instagram4J.responses.direct.IGDirectThreadsResponse;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class IGDirectThreadsUpdateTitleRequest extends IGPostRequest<IGDirectThreadsResponse> {
+    @NonNull
+    private String thread_id;
+    @NonNull
+    private String _title;
+
+    @Override
+    protected IGPayload getPayload() {
+        return new IGPayload() {
+            @Getter
+            private String title = _title;
+        };
+    }
+
+    @Override
+    public String path() {
+        return "direct_v2/threads/" + thread_id + "/update_title/";
+    }
+
+    @Override
+    public Class<IGDirectThreadsResponse> getResponseType() {
+        return IGDirectThreadsResponse.class;
+    }
+
+}
