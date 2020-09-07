c23864dd41c114b8bc2d7fc3b687042bb969b0be
jvogit <justinxvopro@gmail.com>
2020-09-07 14:53:07 -0700

    modify: wrap failed response in feediterable

--- src/main/java/com/github/instagram4j/instagram4j/actions/feed/FeedIterator.java
+++ src/main/java/com/github/instagram4j/instagram4j/actions/feed/FeedIterator.java
@@ -3,6 +3,7 @@ package com.github.instagram4j.instagram4j.actions.feed;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import com.github.instagram4j.instagram4j.IGClient;
+import com.github.instagram4j.instagram4j.exceptions.IGResponseException.IGFailedResponse;
 import com.github.instagram4j.instagram4j.requests.IGPaginatedRequest;
 import com.github.instagram4j.instagram4j.responses.IGPaginatedResponse;
 import lombok.NonNull;
@@ -18,13 +19,13 @@ public class FeedIterator<T extends IGPaginatedResponse> implements Iterator<T> 
 
     @Override
     public boolean hasNext() {
-        return response == null || response.isMore_available();
+        return response == null || (response.getStatusCode() == 200 && response.isMore_available());
     }
 
     @Override
     public T next() {
         if (!hasNext()) throw new NoSuchElementException("No more to request");
-        response = request.execute(client).join();
+        response = request.execute(client).exceptionally(tr -> IGFailedResponse.of(tr.getCause(), request.getResponseType())).join();
         request.setMax_id(response.getNext_max_id());
 
         return response;
--- src/main/java/com/github/instagram4j/instagram4j/requests/IGPaginatedRequest.java
+++ src/main/java/com/github/instagram4j/instagram4j/requests/IGPaginatedRequest.java
@@ -7,6 +7,8 @@ import com.github.instagram4j.instagram4j.responses.IGPaginatedResponse;
 
 public interface IGPaginatedRequest<T extends IGPaginatedResponse> {
     public void setMax_id(String s);
+    
+    public Class<T> getResponseType();
 
     public CompletableFuture<T> execute(IGClient client);
 }
