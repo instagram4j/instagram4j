fc6c5f590aa465135e00fe22949ac1142324d3d2
jvogit <justinxvopro@gmail.com>
2020-09-21 13:11:16 -0700

    add: image_version2 property to TimelineVideoMedia

--- src/main/java/com/github/instagram4j/instagram4j/models/media/timeline/TimelineVideoMedia.java
+++ src/main/java/com/github/instagram4j/instagram4j/models/media/timeline/TimelineVideoMedia.java
@@ -3,6 +3,7 @@ package com.github.instagram4j.instagram4j.models.media.timeline;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.github.instagram4j.instagram4j.models.media.ImageVersions;
 import com.github.instagram4j.instagram4j.models.media.VideoVersionsMeta;
 
 import lombok.Data;
@@ -12,6 +13,7 @@ import lombok.Data;
 @JsonTypeName("2")
 public class TimelineVideoMedia extends TimelineMedia {
     private List<VideoVersionsMeta> video_versions;
+    private ImageVersions image_versions2;
     private long video_duration;
     private boolean has_audio;
     private int original_width;
