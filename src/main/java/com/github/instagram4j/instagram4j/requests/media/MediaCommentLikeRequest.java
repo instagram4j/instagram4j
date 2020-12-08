111079c1952100427dc06a19ac7d55f48350cc3f
jvogit <justinxvopro@gmail.com>
2020-12-08 02:12:30 -0800

    add: media comment like and unlike requests

---
+++ src/main/java/com/github/instagram4j/instagram4j/requests/media/MediaCommentLikeRequest.java
@@ -0,0 +1,35 @@
+package com.github.instagram4j.instagram4j.requests.media;
+
+import com.github.instagram4j.instagram4j.IGClient;
+import com.github.instagram4j.instagram4j.models.IGPayload;
+import com.github.instagram4j.instagram4j.requests.IGPostRequest;
+import com.github.instagram4j.instagram4j.responses.IGResponse;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class MediaCommentLikeRequest extends IGPostRequest<IGResponse> {
+    @NonNull
+    private String _comment_id;
+
+    @Override
+    protected IGPayload getPayload(IGClient client) {
+        return new IGPayload() {
+            @Getter
+            private String comment_id = _comment_id;
+        };
+    }
+
+    @Override
+    public String path() {
+        return String.format("media/%s/comment_like/", _comment_id);
+    }
+
+    @Override
+    public Class<IGResponse> getResponseType() {
+        return IGResponse.class;
+    }
+
+}
