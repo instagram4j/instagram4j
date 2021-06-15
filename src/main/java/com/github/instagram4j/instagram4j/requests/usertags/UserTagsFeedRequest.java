54b0d6aff91b4fa115b7d3aeeb56c11fcf09eeae
unldenis <80055679+unldenis@users.noreply.github.com>
2021-06-15 23:19:25 +0200

    Add files via upload

---
+++ src/main/java/com/github/instagram4j/instagram4j/requests/usertags/UserTagsFeedRequest.java
@@ -0,0 +1,34 @@
+package com.github.instagram4j.instagram4j.requests.usertags;
+
+
+import com.github.instagram4j.instagram4j.IGClient;
+import com.github.instagram4j.instagram4j.requests.IGGetRequest;
+import com.github.instagram4j.instagram4j.requests.IGPaginatedRequest;
+import com.github.instagram4j.instagram4j.responses.usertags.UserTagsFeedResponse;
+
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+public class UserTagsFeedRequest extends IGGetRequest<UserTagsFeedResponse> 
+					implements IGPaginatedRequest {
+	
+    private long userId;
+    
+    @Setter private String max_id ="";
+    
+    @Override
+    public String path() {
+        return "usertags/"+userId+"/feed/";
+    }
+
+    @Override
+    public Class<UserTagsFeedResponse> getResponseType() {
+        return UserTagsFeedResponse.class;
+    }
+
+    @Override
+    public String getQueryString(IGClient client) {
+        return mapQueryString("max_id", max_id);
+    }
+}
