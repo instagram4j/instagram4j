c3ab80748b4a609c28b76554a06edee02f3605a2
mikebgrep <mike2016consult@abv.bg>
2022-12-31 16:42:22 +0200

    added more properties for User

--- src/main/java/com/github/instagram4j/instagram4j/models/user/User.java
+++ src/main/java/com/github/instagram4j/instagram4j/models/user/User.java
@@ -17,6 +17,21 @@ public class User extends Profile {
     private List<ProfilePic> hd_profile_pic_versions;
     private ProfilePic hd_profile_pic_url_info;
     private int account_type;
+    private String public_email;
+    private String contact_phone_number;
+    private String public_phone_country_code;
+    private String public_phone_number;
+    private String business_contact_method;
+    private String business_category_name;
+    private String category_name;
+    private String category;
+    private String address_street;
+    private String city_id;
+    private String city_name;
+    private String latitude;
+    private String longitude;
+    private String zip;
+    private String instagram_location_id;
 
     @Data
     public static class ProfilePic implements Serializable {
