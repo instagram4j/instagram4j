244c31e847c05a541e9a4d25db96b5f045f9336d
Alexsandr Chugunov <aleksander.chugunoff@yandex.ru>
2023-03-30 16:56:50 +0300

    update instagram app version

--- src/main/java/com/github/instagram4j/instagram4j/requests/live/LiveCreateRequest.java
+++ src/main/java/com/github/instagram4j/instagram4j/requests/live/LiveCreateRequest.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class LiveCreateRequest extends IGPostRequest<LiveCreateResponse> {
     private int width = 720, height = 1280;
-    private String broadcastMessage = "test", broadcastType = "RTMP";
+    private String broadcastMessage = "", broadcastType = "RTMP";
 
     @Override
     protected IGPayload getPayload(IGClient client) {
@@ -37,6 +37,10 @@ public class LiveCreateRequest extends IGPostRequest<LiveCreateResponse> {
         private int preview_height;
         private String broadcast_message;
         private String broadcast_type;
+    }
+
+    public void setBroadcastMessage(String broadcastMessage) {
+        this.broadcastMessage = broadcastMessage;
     }
 
 }
